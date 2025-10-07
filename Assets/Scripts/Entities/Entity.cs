b9f370453c3aef840e3c2ac96bb36cbaec5a1ad8
Chpion <golobokov66@gmail.com>
2025-10-07 21:51:26 +0300

    Add Mold scriptableObject. Update Entity

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -1,9 +1,64 @@
+using System;
+using Entities.Molds;
+using Core.ObjectPool;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Entities
 {
-    public class Entity : MonoBehaviour
+    public class Entity : PooledGameObject
     {
+        protected bool LogicIsEnabled { get; private set; }
         
+        [SerializeField] protected UnityEvent OnEnableLogic;
+        [SerializeField] protected UnityEvent OnDisableLogic;
+        [SerializeField] protected Renderer[] renderers;
+
+        public UnityEvent OnExternalActivation;
+        public UnityEvent OnExternalDeactivation;
+        
+        public event Action OnDispose;
+
+        public virtual void LoadEntity(Mold actorMold) { }
+
+        public virtual void ExternalActivation(bool value)
+        {
+            if (value)
+            {
+                OnExternalActivation?.Invoke();
+            }
+            else
+                OnExternalDeactivation?.Invoke();
+        }
+        
+        public virtual void ToggleLogic(bool stateToSet)
+        {
+            LogicIsEnabled = stateToSet;
+            
+            if(stateToSet)
+                OnEnableLogic?.Invoke();
+            else
+                OnDisableLogic?.Invoke();
+        }
+
+        public void ToggleRenderersEnabled(bool stateToSet)
+        {
+            if (renderers == null)
+                return;
+
+            foreach (var rendererComponent in renderers)
+                rendererComponent.enabled = stateToSet;
+        }
+        
+        public override void ReturnToPool() // Unload entity into basic assets
+        {
+            ToggleRenderersEnabled(false);
+            ToggleLogic(false);
+            
+            OnDispose?.Invoke();
+            OnDispose = null;
+
+            base.ReturnToPool();
+        }
     }
 }
---
+++ Assets/Scripts/Entities/Molds/Mold.cs
@@ -0,0 +1,10 @@
+﻿using Core.ObjectPool;
+using UnityEngine;
+
+namespace Entities.Molds
+{
+    public abstract class Mold : ScriptableObject
+    {
+        public abstract PrefabPoolInfo PrefabPoolInfo { get; }
+    }
+}
