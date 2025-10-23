fa3622a2568907ddb6349d5de11c06b77ff05e30
Chpion <golobokov66@gmail.com>
2025-10-24 00:24:56 +0300

    Add new unity layers. Update UnityLayers enum

--- Assets/Scripts/Core/Enums/UnityLayers.cs
+++ Assets/Scripts/Core/Enums/UnityLayers.cs
@@ -2,6 +2,9 @@
 {
     public enum UnityLayers
     {
-        Bounds = 1 << 13
+        Bounds = 1 << 6,
+        PlayerTrigger = 1 << 7,
+        EntityPlayer = 1 << 8,
+        EntityNeutral = 1 << 9,
     }
 }
