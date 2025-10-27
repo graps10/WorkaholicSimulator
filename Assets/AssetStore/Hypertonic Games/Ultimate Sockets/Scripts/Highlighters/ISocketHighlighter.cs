6f77306d42a43b60812760028f4be743d0c1c3a5
Chpion <golobokov66@gmail.com>
2025-10-27 15:38:37 +0200

    Add Ultimate Sockets asset

---
+++ Assets/AssetStore/Hypertonic Games/Ultimate Sockets/Scripts/Highlighters/ISocketHighlighter.cs
@@ -0,0 +1,12 @@
+using Hypertonic.Modules.UltimateSockets.PlaceableItems;
+
+namespace Hypertonic.Modules.UltimateSockets.Highlighters
+{
+    public interface ISocketHighlighter
+    {
+        public void Setup(SocketHighlighter socketHighlighter);
+        public void StartHighlight(PlaceableItem placeableItem);
+        public void StopHighlight();
+        public void SetHologramRotation(PlaceableItem item) { }
+    }
+}
--- Assets/Scripts/Core/Enums/UnityLayers.cs
+++ Assets/Scripts/Core/Enums/UnityLayers.cs
@@ -2,6 +2,7 @@
 {
     public enum UnityLayers
     {
+        UI = 1 << 5,
         Bounds = 1 << 6,
         PlayerTrigger = 1 << 7,
         EntityPlayer = 1 << 8,
