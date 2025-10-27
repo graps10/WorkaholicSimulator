b7fe79da3c72c9ef473d7f3be38356829db83bb9
Chpion <golobokov66@gmail.com>
2025-10-27 15:31:34 +0200

    Add furniture mold

---
+++ Assets/Scripts/Entities/Molds/FurnitureMold.cs
@@ -0,0 +1,14 @@
+﻿using Core.ObjectPool;
+using UnityEngine;
+
+namespace Entities.Molds
+{
+    public class FurnitureMold : SimpleEntityMold
+    {
+        [SerializeField] private PrefabPoolInfo particleOfSocketPlacingPool;
+
+        public PrefabPoolInfo ParticleOfSocketPlacingPool => particleOfSocketPlacingPool;
+        
+        public bool CanBeRotatedBeforePlaced;
+    }
+}
