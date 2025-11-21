532909a66760d441ad901508ac1cfe61b73b465f
Chpion <golobokov66@gmail.com>
2025-11-21 23:24:03 +0200

    Add TestInteractableBox

---
+++ Assets/Scripts/Entities/Interactables/TestInteractableBox.cs
@@ -0,0 +1,12 @@
+﻿using Core.Interfaces;
+using UnityEngine;
+
+namespace Entities.Interactables
+{
+    public class TestInteractableBox : MonoBehaviour, IInteractable
+    {
+        public string InteractionPrompt => "Open Box";
+
+        public void Interact() => Debug.Log("Box Opened! (Interaction Logic works)");
+    }
+}
