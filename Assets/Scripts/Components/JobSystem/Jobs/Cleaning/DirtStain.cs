56d13b4cdad2741cd6ba7cea0a2c7b479f918fc9
Chpion <golobokov66@gmail.com>
2025-12-09 19:35:06 +0200

    Add CleaningTool and DirtStain

---
+++ Assets/Scripts/Components/JobSystem/Jobs/Cleaning/DirtStain.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Components.JobSystem.Jobs.Cleaning
+{
+    public class DirtStain: MonoBehaviour
+    {
+        [SerializeField] private float maxCleanliness = 100f;
+        [SerializeField] private float cleanedDisappearDuration = 0.3f;
+        [SerializeField] private SpriteRenderer spriteRenderer; // or MeshRenderer
+        
+        public bool IsCleaned { get; private set; }
+        
+        private float _currentDirtLevel;
+
+        private void Awake()
+        {
+            _currentDirtLevel = maxCleanliness;
+            if (spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void Clean(float cleanAmount)
+        {
+            if (IsCleaned) return;
+
+            _currentDirtLevel -= cleanAmount;
+            
+            float alpha = Mathf.Clamp01(_currentDirtLevel / maxCleanliness);
+            if (spriteRenderer != null)
+            {
+                Color c = spriteRenderer.color;
+                c.a = alpha;
+                spriteRenderer.color = c;
+            }
+
+            if (_currentDirtLevel <= 0)
+            {
+                IsCleaned = true;
+                OnFullyCleaned();
+            }
+        }
+
+        private void OnFullyCleaned()
+        {
+            transform.DOScale(0f, cleanedDisappearDuration).OnComplete(() => gameObject.SetActive(false));
+        }
+    }
+}
---
+++ Assets/Scripts/Components/JobSystem/Tools/CleaningTool.cs
@@ -0,0 +1,33 @@
+﻿using Components.JobSystem.Jobs.Cleaning;
+using UnityEngine;
+
+namespace Components.JobSystem.Tools
+{
+    public class CleaningTool : JobTool
+    {
+        [Header("Cleaning Settings")] [SerializeField]
+        private float cleaningRate = 50f;
+
+        [SerializeField] private float reachDistance = 2.5f;
+        [SerializeField] private LayerMask dirtLayer;
+        [SerializeField] private Transform rayOrigin;
+
+        protected override void OnToolUsage(bool isInputActive)
+        {
+            // If we don't move and don't press the button - we don't clean
+            if (!isInputActive && !IsMoving()) return;
+            if (Camera.main == null) return;
+            
+            Transform originT = rayOrigin != null ? rayOrigin : Camera.main.transform;
+            Ray ray = new Ray(originT.position, originT.forward);
+
+            // TODO: Add visual effect
+
+            if (Physics.Raycast(ray, out RaycastHit hit, reachDistance, dirtLayer))
+            {
+                if (hit.collider.TryGetComponent(out DirtStain dirt))
+                    dirt.Clean(cleaningRate * Time.deltaTime);
+            }
+        }
+    }
+}
